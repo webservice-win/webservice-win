f6f4eebef5fd26875aa4bdcaa2004283708fa1f1
shihabmoni <shihabmoni5@gmail.com>
2025-01-27 18:46:28 +0600

    file-size updated

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ app.get('/ping', (req, res) => {
 });
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(express.urlencoded({ limit: '10mb', extended: true }))
 app.use(cors(
     {
         origin:[
